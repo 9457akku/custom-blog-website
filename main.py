4cb6c2afcbfb41a23720bf78cb12166a2364991e
pythontechidea <pythontechidea@gmail.com>
2025-11-12 22:27:22 +0530

    initial commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from flask_ckeditor import CKEditor, CKEditorField
 from forms import CreatePostForm, RegForm,CommentForm,LoginForm
 from functools import wraps
 from flask import abort
-
+import os
 '''
 Make sure the required packages are installed: 
 Open the Terminal in PyCharm (bottom left). 
@@ -30,7 +30,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("secret_key")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -52,7 +52,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts_new.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("db_uri",'sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
